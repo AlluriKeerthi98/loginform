9e53dc3dad030cfb475f3c57a13fd41467a21d7c
Alluri keerthi <71062526+AlluriKeerthi98@users.noreply.github.com>
2020-12-14 15:13:29 +0530

    Initial commit

---
+++ src/main/java/com/cg/discover/LoginForm.java
@@ -0,0 +1,13 @@
+package com.cg.discover;
+
+import org.springframework.boot.SpringApplication;    
+import org.springframework.boot.autoconfigure.SpringBootApplication;   
+
+@SpringBootApplication    
+public class LoginForm {
+
+	public static void main(String[] args)   {    
+		
+		SpringApplication.run(LoginForm.class, args);    
+	}   
+}
